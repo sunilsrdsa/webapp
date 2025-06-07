736923662db9b88819feb344bea6e43e1b74ad95
Sunil S R <sunilsrdsa@gmail.com>
2025-06-07 16:22:19 +0530

    read expenses

---
+++ src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,19 @@
+import ExpenseList from "../../components/ExpenseList";
+import useExpenses from "../../hooks/useExpenses";
+
+const Dashboard = () => {
+
+   const{expenses,error,isLoading} = useExpenses();
+  /*  const expenses: Expense[] = [
+       {id: 1, name: "Water bill", amount: 200.00, date: new Date().toDateString(),category: "Utilities",note: "bills"},
+       {id: 2, name: "Electricity bill", amount: 500.00, date: new Date().toDateString(),category: "Utilities",note: "bills"},
+       {id: 3, name: "Wifi bill", amount: 700.00, date: new Date().toDateString(),category: "Utilities",note: "bills"}
+    ]; */
+   return <div> 
+    {isLoading && <p>Loading...</p>}
+    {error && <p>{error}</p>}
+    <ExpenseList expenses={expenses}/>
+   </div>
+};
+
+export default Dashboard
